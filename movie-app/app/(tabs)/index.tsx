a3c4a05a8eb1717b73408e615a2a8789a6e0aa61
Siam Sadman <sadman.bin.anwar.siam@gmail.com>
2025-06-01 18:22:42 +0600

    set up

---
+++ movie-app/app/(tabs)/index.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+import "../global.css";
+import { Link } from "expo-router";
+
+export default function Index() {
+  return (
+    <View className="flex-1 justify-center items-center">
+      <Text className="text-5xl text-primary">Welcome User</Text>
+      <Link href="./profile">Profile</Link>
+    </View>
+  );
+}
